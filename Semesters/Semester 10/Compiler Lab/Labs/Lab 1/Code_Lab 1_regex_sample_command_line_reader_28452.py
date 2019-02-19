3941f0ab81d08f3b0bdfe855118d7c1f1f613b2d
Ahmed Hathout <Ahathout2010@gmail.com>
2019-02-19 19:40:13 +0200

    General

---
+++ Semesters/Semester 10/Compiler Lab/Labs/Lab 1/Code_Lab 1_regex_sample_command_line_reader_28452.py
@@ -0,0 +1,12 @@
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=True, description='Sample Commandline')
+
+    parser.add_argument('--file', action="store", help="path of file to take as input", nargs="?",
+                        metavar="file")
+
+    args = parser.parse_args()
+
+    print(args.file)
